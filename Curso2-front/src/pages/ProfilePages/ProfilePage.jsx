d3e226d02077c7b261f9a5a724b67f40c4171220
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-27 12:00:03 +0100

    subiendole algunas cosas del profile

--- Curso2-front/src/pages/ProfilePages/ProfilePage.jsx
+++ Curso2-front/src/pages/ProfilePages/ProfilePage.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import ProfileService from '../../services/ProfileService';
 import CartService from '../../services/CartService';
+import './ProfilePage.css'
 
 const Profile = () => {
     const [user, setUser] = useState(null);
     const [productos, setProductos] = useState([]);
+    const subtotales = productos.map(producto => producto.product.price * producto.quantity);
+    const totalCarrito = subtotales.reduce((total, subtotal) => total + subtotal, 0);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,23 +35,29 @@ const Profile = () => {
 
     return (
         <div>
-            <h1>User Profile</h1>
+            <h1>User Profile</h1><br></br>
             {user && (
                 <div>
-                    <h2>Welcome, {user.name}!</h2>
-                    <p>Email: {user.email}</p>
-                    {/* Renderizar otros detalles del usuario según sea necesario */}
+                    <h2>Welcome, {user.email}!</h2><br></br>
                 </div>
             )}
-            <h2>Products</h2>
+            <h2>Products</h2> <br></br>
             <div>
-                {productos.map((producto, index) => (
-                    <li key={index}>
-                        <h2>{producto.product.title}</h2>
-                        <p>Precio: ${producto.product.price}</p>
-                        <p>Cantidad: {producto.quantity}</p>
-                    </li>
-                ))}
+                <ul>
+                    {productos.map((producto, index) => (
+                        <li key={index}>
+                            <h2>{producto.product.title}</h2>
+                            <p>Precio: ${producto.product.price}</p>
+                            <p>Cantidad: {producto.quantity}</p>
+                            <img src={producto.product.images[0]} alt={producto.title} style={{ width: '258px', height: '195px' }} />
+                            <br></br>
+                            <p className="subtotal">Subtotal: ${subtotales[index]}</p>
+                            <br></br>
+                        </li>
+                    ))}
+                </ul><br></br>
+                <p class='total'>Total Carrito: ${totalCarrito}</p><br></br>
+                <button className="agregar-carrito">Comprar</button>
             </div>
         </div>
     );
