667167fd567154b33759b71ef5550fc8c24c0040
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-27 10:56:01 +0100

    agregando gitignore

--- Curso2-front/src/services/CartService.js
+++ Curso2-front/src/services/CartService.js
@@ -1,12 +1,19 @@
 import AxiosClient from "./AxiosClient";
 
+const token = localStorage.getItem('authToken');
+
 export default class CartService {
     constructor() {
         this.client = new AxiosClient()
     }
     getCart = () => {
         const requestInfo = {
-            url: `http://localhost:1234/carrito/`
+            url: `http://localhost:1234/carrito/`,
+            config: {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
         }
         return this.client.makeGetRequest(requestInfo)
     }
