40d6b79c0f34964de0c518788b4a361a648d6886
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-27 13:34:32 +0100

    algunos pequenios cambios

--- Curso2-front/src/AppRouter.jsx
+++ Curso2-front/src/AppRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Body from './components/body/body.jsx'
 import LoginForm from "./pages/LoginPages/LoginPage";
 import RegistrationForm from "./pages/RegistratePages/RegistratePage";
-import CartPage from "./pages/CartPages/CartPage"
+// import CartPage from "./pages/CartPages/CartPage"
 import Profile from "./pages/ProfilePages/ProfilePage";
 
 
@@ -11,13 +11,13 @@ const AppRouter = () => {
     return (
         <Router>
             <div className="App">
-                <Switch>
-                    <Route exact path="/" component={Body} />
-                    <Route path="/login" component={LoginForm} />
-                    <Route path="/register" component={RegistrationForm} />
-                    <Route path="/cart" component={CartPage} />
-                    <Route path="/profile" component={Profile} />
-                </Switch>
+                <Routes>
+                    <Route path="/" element={<Body />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegistrationForm />} />
+                    {/* <Route path="/cart" element={<CartPage />} /> */}
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
             </div>
         </Router>
     );
